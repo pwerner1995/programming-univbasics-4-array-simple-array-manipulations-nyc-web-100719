8a6117802862e1d20be161bdeacfe110ae4f0125
Peter Werner <pcw1995@yahoo.com>
2019-09-03 19:46:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,4 +34,21 @@ def using_uniq(array)
   array.uniq 
 end
 
-def 
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
+
+
+
+
+
+
+
